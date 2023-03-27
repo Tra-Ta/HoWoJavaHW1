abdbe815c49cc47030ebee874177d652d0f7e737
Tra-Ta <madtazik@gmail.com>
2023-03-27 19:37:43 +0300

    completed task_4

--- src/main/java/HoWo2/Task_4.java
+++ src/main/java/HoWo2/Task_4.java
@@ -1,4 +1,38 @@
 package HoWo2;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Task_4 {
+    public static void main(String[] args) {
+        String output = generateTestString(100);
+        writeToFile("test.txt", output);
+    }
+
+    private static String generateTestString(int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append("TEST ");
+        }
+        return sb.toString();
+    }
+
+    private static void writeToFile(String fileName, String content) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
